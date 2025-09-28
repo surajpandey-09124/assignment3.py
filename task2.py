f094a9f40a52c31e63bb9c0a323ff4e452fb3e58
surajpandey-09124 <surajpandey09124@gmail.com>
2025-09-28 17:10:39 +0530

    Create task2.py

---
+++ task2.py
@@ -0,0 +1,11 @@
+import math
+
+num = float(input("Enter a number: "))
+
+sqrt_num = math.sqrt(num)
+log_num = math.log(num)
+sine_num = math.sin(num)
+
+print(f"Square root of {num} is: {sqrt_num}")
+print(f"Natural logarithm of {num} is: {log_num}")
+print(f"Sine of {num} (in radians) is: {sine_num}")
